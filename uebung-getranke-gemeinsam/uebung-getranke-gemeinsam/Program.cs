812b0ae954b110ce710de416b10e9b0f0ca1fba6
bastian <bbrugger@schueler.hakzell.at>
2022-04-07 12:44:31 +0200

    added new project 'uebung-getranke-gemeinsam'

---
+++ uebung-getranke-gemeinsam/uebung-getranke-gemeinsam/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace uebung_getranke_gemeinsam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
